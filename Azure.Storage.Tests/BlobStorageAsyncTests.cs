783512844dda312185daa19257db6fe8d22fb9f6
Christos Matskas <chris@towzietyke.co.uk>
2015-01-19 22:14:08 +0000

    Added OSS licence and Unit Tests

--- Azure.Storage.Tests/BlobStorageAsyncTests.cs
+++ Azure.Storage.Tests/BlobStorageAsyncTests.cs
@@ -2,73 +2,74 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Azure.Storage.Interfaces;
 using Xunit;
 
 namespace Azure.Storage.Tests
 {
-    public class BlobStorageTests : IDisposable
+    public class BlobStorageAsyncTests : IDisposable
     {
         private const string ContainerName = "test-container";
         private const string ConnectionString = "UseDevelopmentStorage=true";
-        private readonly IBlobStorage blobStorage;
+        private readonly IBlobStorageAsync blobStorage;
 
-        public BlobStorageTests()
+        public BlobStorageAsyncTests()
         {
-            blobStorage = new BlobStorage(ContainerName, ConnectionString);
+            blobStorage = new BlobStorageAsync(ContainerName, ConnectionString);
         }
 
         public void Dispose()
         {
-            blobStorage.DeleteBlobContainer();
+            blobStorage.DeleteBlobContainerAsync().Wait();
             // Dispose anything else that needs to be cleand out
         }
 
         [Fact]
-        public void CreateBlobFromSreamSucceeds()
+        public async Task CreateBlobFromSreamAsyncSucceeds()
         {
             const string streamBlob = "streamblob";
             var bytes = Encoding.UTF8.GetBytes("hello world");
             var stream = new MemoryStream(bytes);
-            var result = blobStorage.CreateBlockBlob(streamBlob, "text/plain", stream);
+            var result = await blobStorage.CreateBlockBlobAsync(streamBlob, "text/plain", stream);
 
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void CreateBlobFromBytesSucceeds()
+        public async Task CreateBlobFromBytesAsyncSucceeds()
         {
             const string streamBlob = "streamblob";
             var bytes = Encoding.UTF8.GetBytes("hello world");
-            var result = blobStorage.CreateBlockBlob(streamBlob, "text/plain", bytes);
+            var result = await blobStorage.CreateBlockBlobAsync(streamBlob, "text/plain", bytes);
 
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void CreateBlobFromStringSucceeds()
+        public async Task CreateBlobFromStringAsyncSucceeds()
         {
             const string stringBlob = "stringblob";
-            var result = blobStorage.CreateBlockBlob(stringBlob, "text/plain", "hello world");
+            var result = await blobStorage.CreateBlockBlobAsync(stringBlob, "text/plain", "hello world");
 
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void CreateBlobFromFileSucceeds()
+        public async Task CreateBlobFromFileAsyncSucceeds()
         {
             const string fileBlob = "fileblob";
 
-            var result = blobStorage.CreateBlockBlob(fileBlob, "SampleData.txt");
+            var result = await blobStorage.CreateBlockBlobAsync(fileBlob, "SampleData.txt");
 
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void GetBlobReferenceSucceeds()
+        public async Task GetBlobReferenceAsyncSucceeds()
         {
             const string stringBlob = "stringblob";
-            blobStorage.CreateBlockBlob(stringBlob, "text/plain", "hello world");
+            await blobStorage.CreateBlockBlobAsync(stringBlob, "text/plain", "hello world");
 
             var result = blobStorage.GetBlockBlobReference(stringBlob);
 
@@ -76,12 +77,12 @@ namespace Azure.Storage.Tests
         }
 
         [Fact]
-        public void GetBlobContentsAsStreamSucceeds()
+        public async Task GetBlobContentsAsStreamAsyncSucceeds()
         {
             const string stringBlob = "streamblob";
             const string originalValue = "Hello World";
-            blobStorage.CreateBlockBlob(stringBlob, "text/plain", originalValue);
-            var stream = blobStorage.GetBlockBlobDataAsStream(stringBlob);
+            await blobStorage.CreateBlockBlobAsync(stringBlob, "text/plain", originalValue);
+            var stream = await blobStorage.GetBlockBlobDataAsStreamAsync(stringBlob);
             string result;
             using (var reader = new StreamReader(stream, Encoding.UTF8))
             {
@@ -93,25 +94,25 @@ namespace Azure.Storage.Tests
         }
 
         [Fact]
-        public void GetBlobContentsAsStringSucceeds()
+        public async Task GetBlobContentsAsStringAsyncSucceeds()
         {
             const string stringBlob = "stringblob";
             const string originalValue = "Hello World";
-            blobStorage.CreateBlockBlob(stringBlob, "text/plain", originalValue);
-            var result = blobStorage.GetBlockBlobDataAsString(stringBlob);
+            await blobStorage.CreateBlockBlobAsync(stringBlob, "text/plain", originalValue);
+            var result = await blobStorage.GetBlockBlobDataAsStringAsync(stringBlob);
 
             Assert.True(!string.IsNullOrEmpty(result));
             Assert.True(string.Equals(result, originalValue, StringComparison.CurrentCultureIgnoreCase));
         }
 
         [Fact]
-        public void GetBlobsInContainerSucceeds()
+        public async Task GetBlobsInContainerAsyncSucceeds()
         {
             const string stringBlob1 = "blob1";
             const string stringBlob2 = "blob2";
             const string originalValue = "Hello World";
-            blobStorage.CreateBlockBlob(stringBlob1, "text/plain", originalValue);
-            blobStorage.CreateBlockBlob(stringBlob2, "text/plain", originalValue);
+            await blobStorage.CreateBlockBlobAsync(stringBlob1, "text/plain", originalValue);
+            await blobStorage.CreateBlockBlobAsync(stringBlob2, "text/plain", originalValue);
             var result = blobStorage.GetBlockBlobsInContainer(ContainerName).ToList();
 
             Assert.NotNull(result);
@@ -121,16 +122,16 @@ namespace Azure.Storage.Tests
 
 
         [Fact]
-        public void DeleteBlobSucceeds()
+        public async Task DeleteBlobAsyncSucceeds()
         {
             const string blobToDelete = "blobtodelete";
             const string content = "Hello World";
-            blobStorage.CreateBlockBlob(blobToDelete, "text/plain", content);
+            await blobStorage.CreateBlockBlobAsync(blobToDelete, "text/plain", content);
             var createResult = blobStorage.GetBlockBlobReference(blobToDelete);
 
             Assert.NotNull(createResult);
 
-            blobStorage.DeleteBlob(blobToDelete);
+            await blobStorage.DeleteBlobAsync(blobToDelete);
             var deleteResult = blobStorage.GetBlockBlobsInContainer(blobToDelete).ToList();
 
             Assert.True(deleteResult.Count == 0);
